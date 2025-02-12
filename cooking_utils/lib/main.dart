b37e7c7f52ac7ef7d46f69e284586fddc767f61a
BPplays <andymalbp@gmail.com>
2025-02-12 09:07:55 -0800

    themeing

--- cooking_utils/lib/main.dart
+++ cooking_utils/lib/main.dart
@@ -7,11 +7,17 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    const textCol = Color(0xffcdd6f4);
+    const accentCol = Color(0xfff5c2e7);
+    const bgCol = Color(0xff1e1e2e);
+    const sbtxt1Col = Color(0xffbac2de);
+
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Flutter Demo test',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -28,8 +34,20 @@ class MyApp extends StatelessWidget {
         //
         // This works for code too, not just values: Most code changes can be
         // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        // colorScheme: ColorScheme.fromSeed(seedColor: Colors.pink, primary: Colors.black, inversePrimary: Colors.white, surface: Colors.black),
         useMaterial3: true,
+        brightness: Brightness.dark,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(fontSize: 32, fontWeight: FontWeight.bold, color: Color(0xffcdd6f4)),
+          bodyLarge: TextStyle(fontSize: 18, color: textCol),
+          bodyMedium: TextStyle(color: textCol),
+          headlineMedium: TextStyle(color: accentCol),
+        ),
+        appBarTheme: const AppBarTheme(
+          color: Colors.blue,
+          iconTheme: IconThemeData(color: textCol),
+        ),
+        colorScheme: ColorScheme.fromSeed(seedColor: accentCol, primary: accentCol, onSurface: textCol, surface: bgCol, brightness: Brightness.dark).copyWith(brightness: Brightness.dark),
       ),
       home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
@@ -105,8 +123,9 @@ class _MyHomePageState extends State<MyHomePage> {
           // wireframe for each widget.
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            const Text(
+            Text(
               'You have pushed the button this many times:',
+              style: Theme.of(context).textTheme.bodyMedium,
             ),
             Text(
               '$_counter',
